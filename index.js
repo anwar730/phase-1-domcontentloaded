0565290805374e46499fc1aa036f28b3931400a6
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-08-23 02:39:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',function(e){
+    let changeParagraph=document.getElementById('text')
+    changeParagraph.textContent="This is really cool!"
+})
